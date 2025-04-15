f95850773d64799317611925bbee681e58fe4c77
Barkard <juniorpinedafigueroa@gmail.com>
2025-04-14 23:58:47 -0400

    Firts commit

---
+++ src/app/limpieza/limpieza.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-limpieza',
+  imports: [],
+  templateUrl: './limpieza.component.html',
+  styleUrl: './limpieza.component.css'
+})
+export class LimpiezaComponent {
+
+}
